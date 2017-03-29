2cec0add7029d86d540de400fa5d0ddc7a1ee02d
Vladimir Logachev <doit@keemail.me>
2017-03-29 22:17:29 +0700

    named args + optional args task

--- src/main/scala/application/Verstack.scala
+++ src/main/scala/application/Verstack.scala
@@ -1,5 +1,12 @@
 package application
 
-class Family(args:String*) extends App {
+object X extends App {
+}
+
+class Family(args:String*) {
   def familySize():Int = args.size
+}
+
+class FlexibleFamily(mother:String, father:String, children:String*) {
+  def familySize():Int = 2 + children.size
 }
--- src/test/scala/application/VerstackSpec.scala
+++ src/test/scala/application/VerstackSpec.scala
@@ -1,6 +1,7 @@
 package application
 
 import org.scalatest._
+import application._
 
 class VerstackSpec extends FlatSpec with Matchers {
   "Family" should "return correct size" in {
@@ -9,6 +10,18 @@ class VerstackSpec extends FlatSpec with Matchers {
     family1.familySize() shouldEqual 4
     family2.familySize() shouldEqual 3
   }
+  "FlexibleFamily" should "work well" in {
+      val family3 = new FlexibleFamily("Mom", "Dad", "Sally", "Dick")
+      family3.familySize() shouldEqual 4
+      val family4 = new FlexibleFamily("Dad", "Mom", "Harry")
+      family4.familySize() shouldEqual 3
+      val familyNoKids = new FlexibleFamily("Mom", "Dad")
+      familyNoKids.familySize() shouldEqual 2
+
+  }
+
+
+
 }
 
 
