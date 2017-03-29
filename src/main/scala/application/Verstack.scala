f046c9472ba2abcac77ebe41564812d68dcb0635
Vladimir Logachev <doit@keemail.me>
2017-03-29 21:34:43 +0700

    Manifest of sequence[arguments]

--- src/main/scala/application/Verstack.scala
+++ src/main/scala/application/Verstack.scala
@@ -1,29 +1,13 @@
 package application
 
-object Verstack extends App {
-  def matchColor(color: String):String = color match {
-    case "blue"|"green"|"red" => color.toUpperCase
-    case _ => s"UNKNOWN COLOR: ${color}"
+class Sum(args:Int*) {
+  def manOf[T: Manifest](t: T): Manifest[T] = manifest[T]
+  def multipleArgumentsIterable(): Seq[Int] = {
+    manOf(args) // scala.collection.Seq[Int]
+    args // WrappedArray(1, 2, 3, 4)
   }
-  def oneOrTheOther(exp:Boolean):String = exp match {
-    case true => "True!"
-    case _ => "It's false"
-  }
-  def checkTruth(exp1:Boolean, exp2:Boolean):String = Vector(exp1, exp2) match {
-    case Vector(true, true) => "Both are true"
-    case Vector(false, false) => "Both are false"
-    case Vector(true, false) => "First: true, second: false"
-    case Vector(false, true) => "First: false, second: true"
-  }
-  def forecast(percentage: Int):String = percentage match {
-    case 100 => "Sunny"
-    case 80 => "Mostly Sunny"
-    case 50 => "Partly Sunny"
-    case 20 => "Mostly Cloudy"
-    case 0 => "Cloudy"
-    case _ => "Unknown"
-  }
-  Vector(100, 80, 50, 20, 0, 15) map (i => println(forecast(i)))
 }
 
-
+object Verstack extends App {
+  println(new Sum(1,2,3,4).multipleArgumentsIterable())
+}
--- src/test/scala/application/VerstackSpec.scala
+++ src/test/scala/application/VerstackSpec.scala
@@ -5,36 +5,6 @@ import org.scalatest._
 import application.Verstack._
 
 class VerstackSpec extends FlatSpec with Matchers {
-  "matchColor" should "work well" in {
-    matchColor("white") shouldEqual "UNKNOWN COLOR: white"
-    matchColor("blue") shouldEqual "BLUE"
-    matchColor("green") shouldEqual "GREEN"
-    matchColor("red") shouldEqual "RED"
-  }
-  "oneOrTheOther" should "also work well" in {
-    lazy val v = Vector(1)
-    lazy val v2 = Vector(3, 4)
-    oneOrTheOther(v == v.reverse) shouldEqual "True!"
-    oneOrTheOther(v2 == v2.reverse) shouldEqual "It's false"
-  }
-  "checkTruth" should "also work well" in {
-    checkTruth(true || false, true) shouldEqual
-    "Both are true"
-    checkTruth(1 > 0 && -1 < 0, 1 == 2) shouldEqual
-    "First: true, second: false"
-    checkTruth(1 >= 2, 1 >= 1) shouldEqual
-    "First: false, second: true"
-    checkTruth(true && false, false && true) shouldEqual
-    "Both are false"
-  }
-  "forecast" should "also work well" in {
-    forecast(100) shouldEqual "Sunny"
-    forecast(80) shouldEqual "Mostly Sunny"
-    forecast(50) shouldEqual "Partly Sunny"
-    forecast(20) shouldEqual "Mostly Cloudy"
-    forecast(0) shouldEqual "Cloudy"
-    forecast(15) shouldEqual "Unknown"
-  }
 }
 
 
