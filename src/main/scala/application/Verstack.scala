fc02757dcef6d9aef6c2d9b8b8475933bf457ae9
Vladimir Logachev <doit@keemail.me>
2017-03-29 21:57:33 +0700

    task for args

--- src/main/scala/application/Verstack.scala
+++ src/main/scala/application/Verstack.scala
@@ -1,13 +1,5 @@
 package application
 
-class Sum(args:Int*) {
-  def manOf[T: Manifest](t: T): Manifest[T] = manifest[T]
-  def multipleArgumentsIterable(): Seq[Int] = {
-    manOf(args) // scala.collection.Seq[Int]
-    args // WrappedArray(1, 2, 3, 4)
-  }
-}
-
-object Verstack extends App {
-  println(new Sum(1,2,3,4).multipleArgumentsIterable())
+class Family(args:String*) extends App {
+  def familySize():Int = args.size
 }
--- src/test/scala/application/VerstackSpec.scala
+++ src/test/scala/application/VerstackSpec.scala
@@ -2,9 +2,13 @@ package application
 
 import org.scalatest._
 
-import application.Verstack._
-
 class VerstackSpec extends FlatSpec with Matchers {
+  "Family" should "return correct size" in {
+    val family1 = new Family("Mom", "Dad", "Sally", "Dick")
+    val family2 = new Family("Dad", "Mom", "Harry")
+    family1.familySize() shouldEqual 4
+    family2.familySize() shouldEqual 3
+  }
 }
 
 
