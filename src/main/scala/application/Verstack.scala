273ec2e1c4028eaac86f1f3871ab6875fe34602c
Vladimir Logachev <doit@keemail.me>
2017-03-29 22:55:34 +0700

    mapping over flexible args

--- src/main/scala/application/Verstack.scala
+++ src/main/scala/application/Verstack.scala
@@ -10,3 +10,15 @@ class Family(args:String*) {
 class FlexibleFamily(mother:String, father:String, children:String*) {
   def familySize():Int = 2 + children.size
 }
+
+class Cup5 (var percentFull:Int = 0) {
+  val max = 100
+  def increase(changes:Int*):Int = {
+    changes.map(iteration => {
+      percentFull += iteration
+      if (percentFull > max) percentFull = max
+      if (percentFull < 0) percentFull = 0
+    })
+    percentFull
+  }
+}
--- src/test/scala/application/VerstackSpec.scala
+++ src/test/scala/application/VerstackSpec.scala
@@ -17,11 +17,13 @@ class VerstackSpec extends FlatSpec with Matchers {
       family4.familySize() shouldEqual 3
       val familyNoKids = new FlexibleFamily("Mom", "Dad")
       familyNoKids.familySize() shouldEqual 2
-
   }
-
-
-
+  "Cup5" should "work well" in {
+    val cup5 = new Cup5(0)
+    cup5.increase(20, 30, 50, 20, 10, -10, -40, 10, 50) shouldEqual 100
+    cup5.increase(10, 10, -10, 10, 90, 70, -70) shouldEqual 30
+  }
+  
 }
 
 
