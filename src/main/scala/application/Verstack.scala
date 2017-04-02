229a2a6685920a2ec8981f09ae2a7cea7ecf51d2
Vladimir Logachev <doit@keemail.me>
2017-04-02 23:07:37 +0700

    Auxilary constructor + overloaded methods

--- src/main/scala/application/Verstack.scala
+++ src/main/scala/application/Verstack.scala
@@ -3,23 +3,13 @@ package application
 object X extends App {
 }
 
-class Tea2(
-  val decaf: Boolean = false,
-  val milk: Boolean = false,
-  val sugar: Boolean = false,
-  val name: String = "Earl Grey") {
-  
-  def describe():String = {
-    var description = name
-    if (decaf) description += " decaf"
-    if (milk) description += " + milk"
-    if (sugar) description += " + sugar"
-    description
+class ClothesWasher(val modelName: String, val capacity: Double) {
+  def this (modelName: String) {
+    this(modelName, 2.0)
   }
-  def calories():Int = {
-    var count = 0
-    if (milk) count += 100
-    if (sugar) count += 16
-    count
+  def this (capacity: Double) {
+    this("Samsung", capacity)
   }
+  def wash(bleach: Int, softener: Int) = s"Wash used ${bleach} bleach and ${softener} fabric softener"
+  def wash() = "Simple wash"
 }
--- src/test/scala/application/VerstackSpec.scala
+++ src/test/scala/application/VerstackSpec.scala
@@ -5,29 +5,25 @@ import application._
 import application.X._
 
 class ThisApp extends FlatSpec with Matchers {
-  it should "tea" in {
-    val tea = new Tea2
-    tea.describe shouldEqual "Earl Grey"
-    tea.calories shouldEqual 0
-    tea.name shouldEqual "Earl Grey"
+  it should "work with all 2 fields" in {
+    val washer1 = new ClothesWasher ("Indesit", 5.5)
+    washer1.modelName shouldEqual "Indesit"
+    washer1.capacity shouldEqual 5.5
   }
-  it should "lemonZinger" in {
-    val lemonZinger = new Tea2(decaf = true, name="Lemon Zinger")
-    lemonZinger.describe shouldEqual "Lemon Zinger decaf"
-    lemonZinger.calories shouldEqual 0
-    lemonZinger.decaf shouldEqual true
+  it should "work with 1 field" in {
+    val washer2 = new ClothesWasher (5.5)
+    washer2.modelName shouldEqual "Samsung"
+    washer2.capacity shouldEqual 5.5
   }
-  it should "sweetGreen" in {
-    val sweetGreen = new Tea2(name="Jasmine Green", sugar=true)
-    sweetGreen.describe shouldEqual "Jasmine Green + sugar"
-    sweetGreen.calories shouldEqual 16
-    sweetGreen.sugar shouldEqual true
+  it should "work with another 1 field" in {
+    val washer3 = new ClothesWasher ("Indesit")
+    washer3.modelName shouldEqual "Indesit"
+    washer3.capacity shouldEqual 2.0
   }
-  it should "teaLatte" in {
-    val teaLatte = new Tea2(sugar=true, milk=true)
-    teaLatte.describe shouldEqual "Earl Grey + milk + sugar"
-    teaLatte.calories shouldEqual 116
-    teaLatte.milk shouldEqual true
+  it should "have overloaded methods" in {
+    val washer = new ClothesWasher ("LG 100", 3.6)
+    washer.wash(2, 1) shouldEqual "Wash used 2 bleach and 1 fabric softener"
+    washer.wash() shouldEqual "Simple wash"
   }
 }
 
