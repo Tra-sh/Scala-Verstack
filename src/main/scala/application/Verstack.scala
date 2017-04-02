39fce284cf5ecbcfdab9b105fc4211aa7fa254f4
Vladimir Logachev <doit@keemail.me>
2017-04-03 03:09:31 +0700

    String Interpolation

--- src/main/scala/application/Verstack.scala
+++ src/main/scala/application/Verstack.scala
@@ -3,19 +3,31 @@ package application
 object X extends App {
 }
 
-case class Person(first:String, last:String, email:String)
-
-case class Dog(name:String, breed:String)
-
-case class Dimension(var height:Int, var width:Int)
-
-case class SimpleTimeDefault(hours:Int, minutes:Int = 0)  {
-  def subtract(b:SimpleTimeDefault): SimpleTimeDefault = {
-    val diff = (hours * 60 + minutes) - (b.hours * 60 + b.minutes)
-    if (diff > 0) {
-      new SimpleTimeDefault(diff / 60, diff % 60)
-    } else {
-      new SimpleTimeDefault(0, 0)
-    }
+class GardenGnome(val height:Double, val weight:Double, val happy:Boolean) {
+  var painted = true
+  def magic(level:Int):String = s"Poof! $level"
+  def this(height:Double) {
+    this(height, 100.0, true)
   }
+  def this(name:String) = {
+    this(15.0)
+  }
+  def show():String = s"$height $weight $happy $painted"
+  def show(level:Int) = s"${magic(level)} $happy $painted"
+}
+
+
+class LabeledGardenGnome(val height:Double, val weight:Double, val happy:Boolean) {
+  var painted = true
+  var otherOptionsString = s"happy: $happy painted: $painted"
+  def magic(level:Int):String = s"Poof! $level"
+  def this(height:Double) {
+    this(height, 100.0, true)
+  }
+  def this(name:String) = {
+    this(15.0)
+    otherOptionsString = s"$happy $painted"
+  }
+  def show():String = s"height: $height weight: $weight $otherOptionsString"
+  def show(level:Int) = s"${magic(level)} $otherOptionsString"
 }
--- src/test/scala/application/VerstackSpec.scala
+++ src/test/scala/application/VerstackSpec.scala
@@ -5,48 +5,24 @@ import application._
 import application.X._
 
 class ThisApp extends FlatSpec with Matchers {
-  it should "be a Person" in {
-    val p = Person("Jane", "Smile", "jane@smile.com")
-    p.first shouldEqual "Jane"
-    p.last shouldEqual "Smile"
-    p.email shouldEqual "jane@smile.com"
+  it should "have gnomes again" in {
+    val gnome = new GardenGnome(20.0, 110.0, false)
+    gnome.show() shouldEqual "20.0 110.0 false true"
+    val bob = new GardenGnome("Bob")
+    bob.show() shouldEqual "15.0 100.0 true true"
   }
-  it should "live in Vector" in {
-    val people = Vector(
-    Person("Jane","Smile","jane@smile.com"),
-    Person("Ron","House","ron@house.com"),
-    Person("Sally","Dove","sally@dove.com"))
-    people(0).toString shouldEqual "Person(Jane,Smile,jane@smile.com)"
-    people(1).toString shouldEqual "Person(Ron,House,ron@house.com)"
-    people(2).toString shouldEqual "Person(Sally,Dove,sally@dove.com)"
+  it should "do some magick" in {
+    val gnome = new GardenGnome(20.0, 50.0, false)
+    gnome.show(87) shouldEqual "Poof! 87 false true"
+    val bob = new GardenGnome("Bob")
+    bob.show(25) shouldEqual "Poof! 25 true true"
   }
-  it should "have dogs" in {
-    val dogs = Vector(
-      Dog("Fido", "Golden Lab"),
-      Dog("Ruff", "Alaskan Malamute"),
-      Dog("Fifi", "Miniature Poodle"))
-    dogs(0).toString shouldEqual "Dog(Fido,Golden Lab)"
-    dogs(1).toString shouldEqual "Dog(Ruff,Alaskan Malamute)"
-    dogs(2).toString shouldEqual "Dog(Fifi,Miniature Poodle)"
+  it should "be reworked with labels" in {
+    val gnome = new LabeledGardenGnome(20.0, 110.0, false)
+    gnome.show() shouldEqual "height: 20.0 " + "weight: 110.0 happy: false painted: true"
+    val bob = new LabeledGardenGnome("Bob")
+    bob.show() shouldEqual "height: 15.0 weight: 100.0 true true"
   }
-  it should "have dimension" in {
-    val c = new Dimension(5,7)
-    c.height shouldEqual 5
-    c.height = 10
-    c.height shouldEqual 10
-    c.width = 19
-    c.width shouldEqual 19
-  }
-  it should "have Time again" in {
-    val anotherT1 = new SimpleTimeDefault(10, 30)
-    val anotherT2 = new SimpleTimeDefault(9)
-    val anotherST = anotherT1.subtract(anotherT2)
-    anotherST.hours shouldEqual 1
-    anotherST.minutes shouldEqual 30
-    val anotherST2 = new SimpleTimeDefault(10).subtract(new SimpleTimeDefault(9, 45))
-    anotherST2.hours shouldEqual 0
-    anotherST2.minutes shouldEqual 15
-  }
-}
 
+}
 
