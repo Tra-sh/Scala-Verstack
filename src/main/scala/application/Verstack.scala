ff6f94cb1c900885c8840195b95d658d954abec7
Vladimir Logachev <doit@keemail.me>
2017-04-03 00:18:43 +0700

    Class Exercises

--- src/main/scala/application/Verstack.scala
+++ src/main/scala/application/Verstack.scala
@@ -3,13 +3,43 @@ package application
 object X extends App {
 }
 
-class ClothesWasher(val modelName: String, val capacity: Double) {
-  def this (modelName: String) {
-    this(modelName, 2.0)
+class Dimension(var height: Int, var width: Int)
+
+class Info(var name:String, var description:String)
+
+class SimpleTime(val hours:Int, val minutes:Int) {
+  def subtract(b:SimpleTime): SimpleTime = {
+    val diff = (hours * 60 + minutes) - (b.hours * 60 + b.minutes)
+    if (diff > 0) {
+      new SimpleTime(diff / 60, diff % 60)
+    } else {
+      new SimpleTime(0, 0)
+    }
   }
-  def this (capacity: Double) {
-    this("Samsung", capacity)
-  }
-  def wash(bleach: Int, softener: Int) = s"Wash used ${bleach} bleach and ${softener} fabric softener"
-  def wash() = "Simple wash"
 }
+
+class SimpleTimeDefault(val hours:Int, val minutes:Int = 0)  {
+  def subtract(b:SimpleTimeDefault): SimpleTimeDefault = {
+    val diff = (hours * 60 + minutes) - (b.hours * 60 + b.minutes)
+    if (diff > 0) {
+      new SimpleTimeDefault(diff / 60, diff % 60)
+    } else {
+      new SimpleTimeDefault(0, 0)
+    }
+  }
+}
+
+class SimpleTimeAux(val hours:Int = 0, val minutes:Int = 0) {
+  def this(hours:Int) {
+    this(hours, 0)
+  }
+  def subtract(b:SimpleTimeAux): SimpleTimeAux = {
+    val diff = (hours * 60 + minutes) - (b.hours * 60 + b.minutes)
+    if (diff > 0) {
+      new SimpleTimeAux(diff / 60, diff % 60)
+    } else {
+      new SimpleTimeAux(0, 0)
+    }
+  }
+}
+
--- src/test/scala/application/VerstackSpec.scala
+++ src/test/scala/application/VerstackSpec.scala
@@ -5,26 +5,62 @@ import application._
 import application.X._
 
 class ThisApp extends FlatSpec with Matchers {
-  it should "work with all 2 fields" in {
-    val washer1 = new ClothesWasher ("Indesit", 5.5)
-    washer1.modelName shouldEqual "Indesit"
-    washer1.capacity shouldEqual 5.5
+  it should "get and set fields" in {
+    val c = new Dimension(5,7)
+    c.height shouldEqual 5
+    c.height = 10
+    c.height shouldEqual 10
+    c.width = 19
+    c.width shouldEqual 19
   }
-  it should "work with 1 field" in {
-    val washer2 = new ClothesWasher (5.5)
-    washer2.modelName shouldEqual "Samsung"
-    washer2.capacity shouldEqual 5.5
+  it should "info class" in {
+    val info = new Info("stuff", "Something")
+    info.name shouldEqual "stuff"
+    info.description shouldEqual "Something"
+    info.description = "Something else"
+    info.description shouldEqual "Something else"
+    info.name = "This shouldEqual the new name"
+    info.name shouldEqual "This shouldEqual the new name"
   }
-  it should "work with another 1 field" in {
-    val washer3 = new ClothesWasher ("Indesit")
-    washer3.modelName shouldEqual "Indesit"
-    washer3.capacity shouldEqual 2.0
+  it should "time, dude" in {
+    val t1 = new SimpleTime(10, 30)
+    val t2 = new SimpleTime(9, 30)
+    val st = t1.subtract(t2)
+    st.hours shouldEqual 1
+    st.minutes shouldEqual 0
+    val st2 = new SimpleTime(10, 30).subtract(new SimpleTime(9, 45))
+    st2.hours shouldEqual 0
+    st2.minutes shouldEqual 45
+    val st3 = new SimpleTime(9, 30).subtract(new SimpleTime(10, 0))
+    st3.hours shouldEqual 0
+    st3.minutes shouldEqual 0
   }
-  it should "have overloaded methods" in {
-    val washer = new ClothesWasher ("LG 100", 3.6)
-    washer.wash(2, 1) shouldEqual "Wash used 2 bleach and 1 fabric softener"
-    washer.wash() shouldEqual "Simple wash"
+  it should "have more simple time" in {
+    val anotherT1 = new SimpleTimeDefault(10, 30)
+    val anotherT2 = new SimpleTimeDefault(9)
+    val anotherST = anotherT1.subtract(anotherT2)
+    anotherST.hours shouldEqual 1
+    anotherST.minutes shouldEqual 30
+    val anotherST2 = new SimpleTimeDefault(10).
+    subtract(new SimpleTimeDefault(9, 45))
+    anotherST2.hours shouldEqual 0
+    anotherST2.minutes shouldEqual 15
   }
+
+  it should "use aux constructor" in {
+    val auxT1 = new SimpleTimeAux(10, 5)
+    val auxT2 = new SimpleTimeAux(6)
+    val auxST = auxT1.subtract(auxT2)
+    auxST.hours shouldEqual 4
+    auxST.minutes shouldEqual 5
+    val auxST2= new SimpleTimeAux(12).subtract(new SimpleTimeAux(9, 45))
+    auxST2.hours shouldEqual 2
+    auxST2.minutes shouldEqual 15
+    val extraTask = new SimpleTimeAux
+    extraTask.hours shouldEqual 0
+    extraTask.minutes shouldEqual 0
+  }
+  
 }
 
 
