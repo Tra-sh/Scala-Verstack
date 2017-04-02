48d0428c90e9ff7216c86d728ca88d44f5b37534
Vladimir Logachev <doit@keemail.me>
2017-04-03 02:04:09 +0700

    Case Classes

--- src/main/scala/application/Verstack.scala
+++ src/main/scala/application/Verstack.scala
@@ -3,22 +3,13 @@ package application
 object X extends App {
 }
 
-class Dimension(var height: Int, var width: Int)
+case class Person(first:String, last:String, email:String)
 
-class Info(var name:String, var description:String)
+case class Dog(name:String, breed:String)
 
-class SimpleTime(val hours:Int, val minutes:Int) {
-  def subtract(b:SimpleTime): SimpleTime = {
-    val diff = (hours * 60 + minutes) - (b.hours * 60 + b.minutes)
-    if (diff > 0) {
-      new SimpleTime(diff / 60, diff % 60)
-    } else {
-      new SimpleTime(0, 0)
-    }
-  }
-}
+case class Dimension(var height:Int, var width:Int)
 
-class SimpleTimeDefault(val hours:Int, val minutes:Int = 0)  {
+case class SimpleTimeDefault(hours:Int, minutes:Int = 0)  {
   def subtract(b:SimpleTimeDefault): SimpleTimeDefault = {
     val diff = (hours * 60 + minutes) - (b.hours * 60 + b.minutes)
     if (diff > 0) {
@@ -28,18 +19,3 @@ class SimpleTimeDefault(val hours:Int, val minutes:Int = 0)  {
     }
   }
 }
-
-class SimpleTimeAux(val hours:Int = 0, val minutes:Int = 0) {
-  def this(hours:Int) {
-    this(hours, 0)
-  }
-  def subtract(b:SimpleTimeAux): SimpleTimeAux = {
-    val diff = (hours * 60 + minutes) - (b.hours * 60 + b.minutes)
-    if (diff > 0) {
-      new SimpleTimeAux(diff / 60, diff % 60)
-    } else {
-      new SimpleTimeAux(0, 0)
-    }
-  }
-}
-
--- src/test/scala/application/VerstackSpec.scala
+++ src/test/scala/application/VerstackSpec.scala
@@ -5,7 +5,31 @@ import application._
 import application.X._
 
 class ThisApp extends FlatSpec with Matchers {
-  it should "get and set fields" in {
+  it should "be a Person" in {
+    val p = Person("Jane", "Smile", "jane@smile.com")
+    p.first shouldEqual "Jane"
+    p.last shouldEqual "Smile"
+    p.email shouldEqual "jane@smile.com"
+  }
+  it should "live in Vector" in {
+    val people = Vector(
+    Person("Jane","Smile","jane@smile.com"),
+    Person("Ron","House","ron@house.com"),
+    Person("Sally","Dove","sally@dove.com"))
+    people(0).toString shouldEqual "Person(Jane,Smile,jane@smile.com)"
+    people(1).toString shouldEqual "Person(Ron,House,ron@house.com)"
+    people(2).toString shouldEqual "Person(Sally,Dove,sally@dove.com)"
+  }
+  it should "have dogs" in {
+    val dogs = Vector(
+      Dog("Fido", "Golden Lab"),
+      Dog("Ruff", "Alaskan Malamute"),
+      Dog("Fifi", "Miniature Poodle"))
+    dogs(0).toString shouldEqual "Dog(Fido,Golden Lab)"
+    dogs(1).toString shouldEqual "Dog(Ruff,Alaskan Malamute)"
+    dogs(2).toString shouldEqual "Dog(Fifi,Miniature Poodle)"
+  }
+  it should "have dimension" in {
     val c = new Dimension(5,7)
     c.height shouldEqual 5
     c.height = 10
@@ -13,54 +37,16 @@ class ThisApp extends FlatSpec with Matchers {
     c.width = 19
     c.width shouldEqual 19
   }
-  it should "info class" in {
-    val info = new Info("stuff", "Something")
-    info.name shouldEqual "stuff"
-    info.description shouldEqual "Something"
-    info.description = "Something else"
-    info.description shouldEqual "Something else"
-    info.name = "This shouldEqual the new name"
-    info.name shouldEqual "This shouldEqual the new name"
-  }
-  it should "time, dude" in {
-    val t1 = new SimpleTime(10, 30)
-    val t2 = new SimpleTime(9, 30)
-    val st = t1.subtract(t2)
-    st.hours shouldEqual 1
-    st.minutes shouldEqual 0
-    val st2 = new SimpleTime(10, 30).subtract(new SimpleTime(9, 45))
-    st2.hours shouldEqual 0
-    st2.minutes shouldEqual 45
-    val st3 = new SimpleTime(9, 30).subtract(new SimpleTime(10, 0))
-    st3.hours shouldEqual 0
-    st3.minutes shouldEqual 0
-  }
-  it should "have more simple time" in {
+  it should "have Time again" in {
     val anotherT1 = new SimpleTimeDefault(10, 30)
     val anotherT2 = new SimpleTimeDefault(9)
     val anotherST = anotherT1.subtract(anotherT2)
     anotherST.hours shouldEqual 1
     anotherST.minutes shouldEqual 30
-    val anotherST2 = new SimpleTimeDefault(10).
-    subtract(new SimpleTimeDefault(9, 45))
+    val anotherST2 = new SimpleTimeDefault(10).subtract(new SimpleTimeDefault(9, 45))
     anotherST2.hours shouldEqual 0
     anotherST2.minutes shouldEqual 15
   }
-
-  it should "use aux constructor" in {
-    val auxT1 = new SimpleTimeAux(10, 5)
-    val auxT2 = new SimpleTimeAux(6)
-    val auxST = auxT1.subtract(auxT2)
-    auxST.hours shouldEqual 4
-    auxST.minutes shouldEqual 5
-    val auxST2= new SimpleTimeAux(12).subtract(new SimpleTimeAux(9, 45))
-    auxST2.hours shouldEqual 2
-    auxST2.minutes shouldEqual 15
-    val extraTask = new SimpleTimeAux
-    extraTask.hours shouldEqual 0
-    extraTask.minutes shouldEqual 0
-  }
-  
 }
 
 
