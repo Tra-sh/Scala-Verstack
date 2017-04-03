4c26995760580496b4ff188cfe4034b458cbb39b
Vladimir Logachev <doit@keemail.me>
2017-04-03 18:43:09 +0700

    pattern matching for checking int of Range

--- src/main/scala/application/Verstack.scala
+++ src/main/scala/application/Verstack.scala
@@ -1,29 +1,8 @@
 package application
 
 object X extends App {
-  def explicitDouble(d1:Double, d2:Double, d3:Double):Vector[Double] = Vector(d1, d2, d3)
-
-  def explicitList(src:Vector[Double]):List[Double] = src.toList;
-
-  def explicitSet(src:Vector[Double]):Set[Double] = src.toSet;
-
-  def forecast(percentage: Int):String = percentage match {
-     case 100 => "Sunny"
-     case 80 => "Mostly Sunny"
-     case 50 => "Partly Sunny"
-     case 20 => "Mostly Cloudy"
-      case 0 => "Cloudy"
-      case _ => "Unknown"
-  }
-
-  def historicalData(a:Vector[Int]):String = {
-    a.groupBy(a => a).mapValues(_.size)
-    .toList.sortBy(- _._1)
-    .map { case (weatherType, count) => s"${forecast(weatherType)}=$count"}
-    .mkString(", ")
-    .toString
-  }
 
 }
+
 
 
--- src/test/scala/application/VerstackSpec.scala
+++ src/test/scala/application/VerstackSpec.scala
@@ -5,19 +5,12 @@ import application._
 import application.X._
 
 class ThisApp extends FlatSpec with Matchers {
-  it should "return a vector of double" in {
-    explicitDouble(1.0, 2.0, 3.0) shouldEqual Vector(1.0, 2.0, 3.0)
-  }
-  it should "do some extra work for me" in {
-    explicitList(Vector(10.0, 20.0)) shouldEqual List(10.0, 20.0)
-    explicitList(Vector(1, 2, 3)) shouldEqual List(1.0, 2.0, 3.0)
-    explicitSet(Vector(10.0, 20.0, 10.0)) shouldEqual Set(10.0, 20.0)
-    explicitSet(Vector(1, 2, 3, 2, 3, 4)) shouldEqual Set(1.0, 2.0, 3.0, 4.0)
-  }
-  it should "pattern matching" in {
-    val weather = Vector(100, 80, 20, 100, 20)
-    println(historicalData(weather))
-    historicalData(weather) shouldEqual "Sunny=2, Mostly Sunny=1, Mostly Cloudy=2"
+  it should "use pattern matching for checking int of Range" in {
+    val between = (temperature:Int, low:Int, high:Int) => temperature match {
+      case temp if `low` until `high` contains temp => true
+      case _ => false
+    }
+    between(70, 80, 90) shouldEqual false
+    between(70, 60, 90) shouldEqual true
   }
 }
-
