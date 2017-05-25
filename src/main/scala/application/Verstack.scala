735d5bb3c7eb5b8507b4f3c62d964f7ae064f5e5
Vladimir Logachev <doit@keemail.me>
2017-05-26 00:01:57 +0700

    Factorial, PrintWriter, File

--- src/main/scala/application/Verstack.scala
+++ src/main/scala/application/Verstack.scala
@@ -10,11 +10,19 @@ object X extends App {
   }
 
   @scala.annotation.tailrec
-  def factorial(x:BigInt, accumulator:BigInt = 1):BigInt = x match {
-    case x:BigInt if x == 1 => accumulator
-    case x:BigInt if x > 1 => factorial(x - 1, x * accumulator)
-    case _ => 0
+  def factorial(x:BigInt, acc:BigInt = 1):BigInt = x match {
+    case x:BigInt if x <= 1 => acc
+    case x:BigInt if x > 1 => factorial(x - 1, x * acc)
   }
 
   //time {factorial(100000)}
+  val content = factorial(1000000).toString
+  val filename = "factorial-scala.txt"
+
+  // PrintWriter
+  import java.io._
+  val pw = new PrintWriter(new File(filename))
+  pw.write(content)
+  pw.close
+
 }
