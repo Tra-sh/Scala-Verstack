80dbd14d128ecd3df27b7b978d0cb63b9a492b32
Vladimir Logachev <doit@keemail.me>
2017-05-25 19:26:52 +0700

    Factorial+Timer

--- src/main/scala/application/Verstack.scala
+++ src/main/scala/application/Verstack.scala
@@ -2,13 +2,19 @@ package application
 
 object X extends App {
 
+  def time[R](block: => R): Unit = {  
+    val t0 = System.currentTimeMillis()
+    val result = block    // call-by-name
+    val t1 = System.currentTimeMillis()
+    println(s"Execution time: ${t1 - t0} ms")
+  }
+
   @scala.annotation.tailrec
   def factorial(x:BigInt, accumulator:BigInt = 1):BigInt = x match {
     case x:BigInt if x == 1 => accumulator
     case x:BigInt if x > 1 => factorial(x - 1, x * accumulator)
     case _ => 0
   }
+
+  //time {factorial(100000)}
 }
-
-
-
