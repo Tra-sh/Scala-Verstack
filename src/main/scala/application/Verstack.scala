0ab95f866fb21850aca685bb58c81fe0f5a60017
The Man <doit@keemail.me>
2017-08-01 08:15:02 +0700

    tests work again

--- src/main/scala/application/Verstack.scala
+++ src/main/scala/application/Verstack.scala
@@ -1,48 +1,5 @@
 package application
 
 object X extends App {
-  import java.io._ 
-  val file = "4.html"
-  val campagnCode = "1"
-
-  def makeHTML(list: List[(String, String)]) = {
-    val header = "<html><body><ol>"
-    val footer = "</ol></body></html>"
-    val items = list.map(s => s"""<li><a href="https://m.vk.com/${s._1}?${campagnCode}" target="_blank">${s._2}</a></li>""")
-    ((header :: items) :+ footer).mkString
-  }
-
-  def collectIds(string: String) = {
-    val vkontakteId = """<a class="si_owner[^>]*href="/([^"]+)">([^<]+)</a>""".r
-    for {
-      vkontakteId(id,name) <- (vkontakteId findAllIn string)
-    } yield (id,name)
-  }
-
-  def read(filename: String) = {
-    import java.nio.charset.CodingErrorAction
-    import scala.io.Codec
-    implicit val codec = Codec("UTF-8")
-    codec.onMalformedInput(CodingErrorAction.REPLACE)
-    codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
-    try {
-      Some(io.Source.fromFile(filename).getLines.mkString)
-    } catch {
-      case ex: FileNotFoundException => {println(s"Couldn't FIND '${filename}'"); None}
-      case ex: IOException => {println(s"Couldn't READ '${filename}'"); None}
-    }
-  }
-
-  def write(content: String, filename: String) = {
-    import java.nio.file.{Paths, Files}
-    import java.nio.charset.StandardCharsets
-    Files.write(Paths.get(filename), content.getBytes(StandardCharsets.UTF_8))
-    println(s"Done writing ${filename}!")
-  }
-
-  read(file) match {
-    case Some(s) => write(makeHTML(collectIds(s).toList), s"updated_${file}")
-    case _ => ()
-  }
-
+  def calculate(i:Int) = i + 1
 }
