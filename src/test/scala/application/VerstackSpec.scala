f3a0b8f860d6bd95df79a45eea5ce418ba1beed1
The Man <doit@keemail.me>
2017-08-02 08:09:42 +0700

    Pattern Matching Case Classes

--- src/test/scala/application/VerstackSpec.scala
+++ src/test/scala/application/VerstackSpec.scala
@@ -17,6 +17,9 @@ class ThisApp extends FlatSpec with Matchers {
 
     travel(trip2(1)) shouldEqual "Plane B757 " + "Vector(Passenger(Harvey,Rabbit), " + "Passenger(Dorothy,Gale))"
     travel2(Passenger("Sally", "Marie")) shouldEqual "Sally is walking"
+
+    case class Kitten(name:String)
+    travel2(Kitten("Kitty")) shouldEqual "Kitten(Kitty) is in limbo!"
     }
 
 }
