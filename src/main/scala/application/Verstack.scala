db2afb5d6c5639497a14777a850a6a9d7aecebad
The Man <doit@keemail.me>
2017-08-03 01:14:26 +0700

    refactoring

--- src/main/scala/application/Verstack.scala
+++ src/main/scala/application/Verstack.scala
@@ -6,23 +6,6 @@ object X extends App {
   case class Plane(passengers: Vector[Passenger], name: String)
   case class Passenger(first: String, last: String)
 
-  def travel(transport:Any):String = {
-    transport match {
-      case Train(travelers, line) => s"Train line $line $travelers"
-      case Bus(travelers, seats) => s"Bus size $seats $travelers"
-      case Plane(passengers, name) => s"Plane $name $passengers"
-      case Passenger => "Walking along"
-      case what => s"$what is in limbo!"
-    }
-  }
-
-  def travel2(transport:Any):String = {
-    transport match {
-      case Train(travelers, line) => s"Train line $line $travelers"
-      case Bus(travelers, seats) => s"Bus size $seats $travelers"
-      case Plane(passengers, name) => s"Plane $name $passengers"
-      case Passenger(first, last) => s"$first is walking"
-      case what => s"$what is in limbo!"
-    }
-  }
+  def assignResult(arg:Boolean) = if(arg) 42 else 47
+  
 }
--- src/test/scala/application/VerstackSpec.scala
+++ src/test/scala/application/VerstackSpec.scala
@@ -7,19 +7,8 @@ import application.X._
 class ThisApp extends FlatSpec with Matchers {
   
   it should "Pattern Matching Case Classes" in {
-    val travelers = Vector(
-      Passenger("Harvey", "Rabbit"),
-      Passenger("Dorothy", "Gale"))
-    val trip = Vector(
-      Train(travelers, "Reading"),
-      Bus(travelers, 100))
-    val trip2 = Vector(Train(travelers, "Reading"), Plane(travelers, "B757"), Bus(travelers, 100))
-
-    travel(trip2(1)) shouldEqual "Plane B757 " + "Vector(Passenger(Harvey,Rabbit), " + "Passenger(Dorothy,Gale))"
-    travel2(Passenger("Sally", "Marie")) shouldEqual "Sally is walking"
-
-    case class Kitten(name:String)
-    travel2(Kitten("Kitty")) shouldEqual "Kitten(Kitty) is in limbo!"
-    }
+    assignResult(true) shouldEqual 42
+    assignResult(false) shouldEqual 47
+  }
 
 }
