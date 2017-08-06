746aaac910dc5e2afcde05022433fa29f8b2c1e0
The Man <doit@keemail.me>
2017-08-06 12:29:32 +0700

    Base Class

--- src/main/scala/application/Verstack.scala
+++ src/main/scala/application/Verstack.scala
@@ -1,25 +1,99 @@
 package application
 
 object X extends App {
-  
-  class GreatApe {
-    val weight = 100.0
-    val age = 12
 
-    def vocalize = "Grrr!"
+  class GreatApe(
+    val weight: Double, 
+    val age: Int, 
+    val gender: String = "Female") {
+    def myWords = Vector("Roar")
   }
 
-  class Bonobo extends GreatApe
-  class Chimpanzee extends GreatApe
-  class BonoboB extends Bonobo
+  class Bonobo(weight:Double, age:Int)
+    extends GreatApe(weight, age)
 
-  def says(monk: GreatApe) = s"says ${monk.vocalize}"
+  class Chimpanzee(weight:Double, age:Int)
+    extends GreatApe(weight, age)
 
-  class Cycle {
-    val wheels = 2
-    def ride = "Riding"
+  class BonoboB(weight:Double, age:Int)
+    extends Bonobo(weight, age)
+
+  class BonoboC(weight:Double, age:Int) 
+    extends BonoboB(weight, age)
+
+  def display(ape:GreatApe) = s"weight: ${ape.weight} age: ${ape.age}" 
+
+  class House(
+    val address:String,
+    val state:String, 
+    val zip:String) {
+
+    def this(state:String, zip:String) = this("address?", state, zip)
+    def this(zip:String) = this("address?", "state?", zip)
   }
 
-  class Bicycle extends Cycle
+  class Home
+    extends House("00000") {
+    val heart = true
+    override def toString = "Where the heart is"
+  }
+
+  class VacationHouse(
+    state: String, 
+    zip: String, 
+    val startMonth: Int, 
+    val endMonth:Int) 
+    extends House(state, zip) {
+
+      def monthNameByNumber(n: Int) = n match {
+        case 1 => "Jan"
+        case 2 => "F"
+        case 3 => "Mr"
+        case 4 => "Ap"
+        case 5 => "May"
+        case 6 => "June"
+        case 7 => "Jly"
+        case 8 => "August"
+        case 9 => "S"
+        case 10 => "O"
+        case 11 => "N"
+        case 12 => "D"
+      }
+
+      val start = monthNameByNumber(startMonth)
+      val end = monthNameByNumber(endMonth)
+      override def toString = s"Rented house in MI for months of ${start} through ${end}."
+    }
+
+  class TreeHouse(
+    val name:String, zip:String)
+    extends House(zip)
+
+  class Trip(
+    val origination: String, 
+    val destination: String, 
+    val startDate: String,
+    val endDate: String) {
+
+    override def toString = s"From ${origination} to ${destination}: ${startDate} to ${endDate}"
+    }
+
+  class AirplaneTrip(
+    origination: String, 
+    destination: String, 
+    startDate: String,
+    endDate: String,
+    movie: String) extends Trip(origination, destination, startDate, endDate)
     
+  class CarTrip(
+    origination: String, 
+    destination: String, 
+    startDate: String,
+    endDate: String) extends Trip(origination, destination, startDate, endDate) {
+
+    def this(
+      listOfCities: Vector[String], 
+      startDate: String, 
+      endDate: String) = this(listOfCities.head, listOfCities.last, startDate, endDate)
+  }
 }
--- src/test/scala/application/VerstackSpec.scala
+++ src/test/scala/application/VerstackSpec.scala
@@ -6,32 +6,36 @@ import application.X._
 
 class ThisApp extends FlatSpec with Matchers {
 
-  it should "vocalize" in {
-    val ape1 = new GreatApe
-    ape1.vocalize shouldEqual "Grrr!"
-    val ape2 = new Bonobo
-    ape2.vocalize shouldEqual "Grrr!"
-    val ape3 = new Chimpanzee
-    ape3.vocalize shouldEqual "Grrr!"
+  it should "Base Class apes" in {
+    val c = new BonoboC(111, 222) 
+    c.weight shouldEqual 111
+    c.age shouldEqual 222
+
+    val roaringApe = new GreatApe(112, 9, "Male")
+    roaringApe.myWords shouldEqual Vector("Roar")
+  }
+
+  it should "Base Class houses" in {
+    val h = new Home
+    h.toString shouldEqual "Where the heart is"
+    h.heart shouldEqual true
+
+    val v = new VacationHouse("MI","49431",6,8)
+    v.toString shouldEqual "Rented house in MI for months of " + "June through August."
   }
   
-  it should "says" in {
-    says(new GreatApe) shouldEqual "says Grrr!"
-    says(new Bonobo) shouldEqual "says Grrr!"
-    says(new Chimpanzee) shouldEqual "says Grrr!"
-    says(new BonoboB) shouldEqual "says Grrr!"
+  it should "Base Class trip" in {
+    val t = new Trip("Detroit","Houston", "5/1/2012","6/1/2012")
+    val a = new AirplaneTrip("Detroit", "London","9/1/1939", "10/31/1939","Superman")
+
+    val cities = Vector("Boston", "Albany","Buffalo","Cleveland", "Columbus","Indianapolis", "St. Louis", "Kansas City", "Denver","Grand Junction", "Salt Lake City", "Las Vegas", "Bakersfield","San Francisco")
+
+    val c = new CarTrip(cities, "6/1/2012","7/1/2012")
+
+    c.origination shouldEqual "Boston"
+    c.destination shouldEqual "San Francisco"
+    c.startDate shouldEqual "6/1/2012"
+    t.toString shouldEqual "From Detroit to Houston:" + " 5/1/2012 to 6/1/2012"
+    c.toString shouldEqual "From Boston to San Francisco:" + " 6/1/2012 to 7/1/2012"
   }
-
-  it should "cycles" in {
-    val c = new Cycle
-    c.ride shouldEqual "Riding"
-    val b = new Bicycle
-    b.ride shouldEqual "Riding"
-    b.wheels shouldEqual 2
-  }
-  
-
-
-
-
 }
