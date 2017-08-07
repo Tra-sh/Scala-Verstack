3bfce1fe90c27e7022f7a91fe2eb501b2a410269
The Man <doit@keemail.me>
2017-08-07 10:22:36 +0700

    Enumerations levels

--- src/main/scala/application/Verstack.scala
+++ src/main/scala/application/Verstack.scala
@@ -1,44 +1,18 @@
 package application
 
 object XX extends App {
-
-  object MonthName extends Enumeration {
-    type MonthName = Value
-    val January = Value(1)
-    val February, March, April, May, June, July, August, September, October, November, December = Value
-
-    def monthNumber(n: MonthName) = n.id
-
-    def season(n: MonthName) = n.id match {
-      case 12 | 1 | 2 => "Winter"
-      case 3 | 4 | 5 => "Spring"
-      case 6 | 7 | 8 => "Summer"
-      case 9 | 10 | 11 => "Autumn"
-    }
+  object Level extends Enumeration {
+    type Level = Value
+    val Overflow, High, Medium, Low, Empty = Value
+    val Draining, Pooling, Dry = Value
   }
 
-  object Entry extends Enumeration {
-    type Entry = Value
-    val Nope, X, O = Value
-  }
-  import Entry._
+  import Level._
 
-  case object Cell {
-    var entry = Entry.Nope
-    def set(e: Entry) = e match {
-      case X | O if entry == Nope => {
-        entry = e; 
-        "successful move"
-      }
-      case _ => "invalid move"
-    }
-  } 
-
-  class Grid {
-    val cells = Vector.fill(3,3)(Cell)
-    def play(e:Entry , x:Int, y:Int):String = 
-      if ((0 <= x && x <= 2) && (0 <= y && y <= 2)) 
-        cells(x)(y).set(e)
-      else "invalid move"
+  def checkLevel(level:Level)= level match {
+    case Overflow => ">>> Overflow!"
+    case Empty | Dry => "Alert"
+    case Pooling => "Warning!"
+    case other =>  s"Level $level OK"
   }
 }
--- src/test/scala/application/VerstackSpec.scala
+++ src/test/scala/application/VerstackSpec.scala
@@ -6,34 +6,8 @@ import application.XX._
 
 class ThisApp extends FlatSpec with Matchers {
   
-  it should "Enumerations MonthName" in {
-    MonthName.February.toString shouldEqual "February"
-    MonthName.February.id shouldEqual 2
-    MonthName.December.id shouldEqual 12
-    MonthName.July.id shouldEqual 7
-
-    import MonthName._
-    July.toString shouldEqual "July"
-    monthNumber(July) shouldEqual 7
-  }
-
-  it should "Enumerations seasons" in {
-    import MonthName._
-    season(January) shouldEqual "Winter"
-    season(April) shouldEqual "Spring"
-    season(August) shouldEqual "Summer"
-    season(November) shouldEqual "Autumn"
-  }
-
-  it should "Enumerations grid" in {
-    val grid = new Grid
-    import application.XX.Entry._
-    grid.play(X, 1, 1) shouldEqual "successful move"
-    grid.play(X, 1, 1) shouldEqual "invalid move"
-    grid.play(O, 1, 3) shouldEqual "invalid move"
-  } 
-
-  it should "Enumerations Levels" in {
+  it should "Enumerations levels" in {
+    import application.XX.Level._
     Level.Draining shouldEqual Draining
     Level.Draining.id shouldEqual 5
     checkLevel(Low) shouldEqual "Level Low OK"
