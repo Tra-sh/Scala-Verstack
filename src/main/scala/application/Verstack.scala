8952c8a7b560928709cbb0c9b9dd807853c93971
The Man <doit@keemail.me>
2017-11-03 14:44:20 +0700

    roots

--- src/main/scala/application/Verstack.scala
+++ src/main/scala/application/Verstack.scala
@@ -1,14 +1,15 @@
 package application
 
 object XX extends App {
-  def isGoodEnough(x:Double, guess: Double) = 
-    Math.round(guess*guess*1000).toDouble/1000 == x
 
+  def isGoodEnough(x: Double, guess: Double) = 
+    //(guess*guess - x).abs < 0.000000000000001
+    guess*guess == x
 
-  def improve(x:Double, guess: Double) = 
+  def improve(x: Double, guess: Double) = 
     (x / guess + guess) / 2 
 
   def sqrtIter(x: Double, guess: Double = 1): Double =
-    if (isGoodEnough(x, guess)) Math.round(guess*10000).toDouble/10000
+    if (isGoodEnough(x, guess)) guess
     else sqrtIter(x, improve(x, guess))
 }
--- src/test/scala/application/VerstackSpec.scala
+++ src/test/scala/application/VerstackSpec.scala
@@ -8,9 +8,10 @@ import application.XX._
 class ThisApp extends FlatSpec with Matchers {
   
   it should "sqrtIter" in {
-    sqrtIter(2) shouldEqual 1.4142
+    sqrtIter(2) shouldEqual 1.414213562373095
     sqrtIter(4) shouldEqual 2
     sqrtIter(10000) shouldEqual 100
+    sqrtIter(0.001) shouldEqual 0.03162277660168379
   }
 
 }
