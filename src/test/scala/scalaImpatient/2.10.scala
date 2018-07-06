60ce90691143c0a20914575de9bd62d277122140
Vladimir Logachev <doit@keemail.me>
2018-07-06 19:30:58 +0700

    better ignore for tests

--- src/test/scala/scalaImpatient/2.10.scala
+++ src/test/scala/scalaImpatient/2.10.scala
@@ -2,15 +2,10 @@ package scalaImpatient
 import org.scalatest._
 
 object Exercise {
-  println(Math.pow(1, 0.5))
-  println(Math.pow(2, 0.5))
-  println(Math.pow(4, 0.5))
-  println(Math.pow(8, 0.5))
   def pow(x: Double, n: Double): Double = n match {
     case 0 => 1
     case n if n < 0 => 1 / pow(x, Math.abs(n))
     case n if n >= 1 => x * pow(x, n-1)
-    // case n => (pow(x, n/2)) * (pow(x, n/2))
   }
 }
 
@@ -35,9 +30,8 @@ class ExerciseSpec extends FlatSpec with Matchers {
     pow(2, -3) shouldBe 0.125
     pow(4, -4) shouldBe 0.00390625
   }
-  // it should "calculate positive pow less than 1" in {
-  //   pow(1, 0.5) shouldBe 1
-  //   pow(2, 0.5) shouldBe 0.5
-  //   pow(4, 0.5) shouldBe 0.255
-  // }
+  ignore should "calculate positive pow less than 1" in {
+    pow(1, 0.5) shouldBe 1
+    pow(4, 0.5) shouldBe 2
+  }
 }
